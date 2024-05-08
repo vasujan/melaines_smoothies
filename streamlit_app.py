7797cecee9468742bd9ca51589e59d9066e40f58
Vasu Jain <36842328+vasujan@users.noreply.github.com>
2024-05-08 22:21:05 +0530

    debug app: write pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 fruits_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 pd_df = fruits_df.to_pandas()
-
+st.write(pd_df)
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -29,7 +29,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-    st.stop()
     for fruit_chosen in ingredients_list:
         st.subheader(f"{fruit_chosen} Nutrition Information")
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
