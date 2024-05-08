669f6fc82198ccb609bb04f44faf6d3ddd315278
Vasu Jain <36842328+vasujan@users.noreply.github.com>
2024-05-08 22:18:27 +0530

    update app: fix pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
 cnx = st.connection("snowflake")
 session = cnx.session()
 fruits_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
+pd_df = fruits_df.to_pandas()
+
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
@@ -30,7 +32,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         st.subheader(f"{fruit_chosen} Nutrition Information")
-        search_on = fruits_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
